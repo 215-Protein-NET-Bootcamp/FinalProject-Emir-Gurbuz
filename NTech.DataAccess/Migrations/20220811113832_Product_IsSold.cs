ac7e3a855002d38d8345e72cd034be7ac285bd37
emir57 <emir.gurbuz06@hotmail.com>
2022-08-11 14:39:19 +0300

    Product_IsSold Migration

---
+++ NTech.DataAccess/Migrations/20220811113832_Product_IsSold.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NTech.DataAccess.Migrations
+{
+    public partial class Product_IsSold : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsSold",
+                table: "Products",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsSold",
+                table: "Products");
+        }
+    }
+}
