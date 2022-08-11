3ca73f8b94d3b34d0645b050d110cba0177b8160
emir57 <emir.gurbuz06@hotmail.com>
2022-08-11 16:22:49 +0300

    Complete SetUserId Test

---
+++ NTech.Test/Extension/ExtensionsTest.cs
@@ -0,0 +1,33 @@
+﻿using Core.Extensions;
+using NTech.Entity.Concrete;
+
+namespace NTech.Test.Extension
+{
+    public class ExtensionsTest
+    {
+        public ExtensionsTest()
+        {
+
+        }
+        [Theory]
+        [InlineData(1)]
+        public void Set_user_id_value(int userId)
+        {
+            Product product = new();
+
+            product.SetUserId(userId);
+
+            Assert.Equal(userId, product.UserId);
+        }
+        [Theory,InlineData(1)]
+        public void Set_user_id_null_exception(int userId)
+        {
+            Assert.Throws(typeof(ArgumentNullException), () =>
+            {
+                Category category = new();
+
+                category.SetUserId(userId);
+            });
+        }
+    }
+}
