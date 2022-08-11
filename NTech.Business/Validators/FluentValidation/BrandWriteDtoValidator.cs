cb77ea89a6f56fb29a8bd579f5b710c7a985a239
emir57 <emir.gurbuz06@hotmail.com>
2022-08-11 22:09:20 +0300

    BrandWriteDtoValidator And Use

---
+++ NTech.Business/Validators/FluentValidation/BrandWriteDtoValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using NTech.Dto.Concrete;
+
+namespace NTech.Business.Validators.FluentValidation
+{
+    public class BrandWriteDtoValidator : AbstractValidator<BrandWriteDto>
+    {
+        public BrandWriteDtoValidator()
+        {
+            RuleFor(b => b.Name)
+                .NotNull()
+                .NotEmpty();
+            RuleFor(b => b.Name)
+                .MaximumLength(50);
+        }
+    }
+}
