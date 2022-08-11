9818340fbfe0c1610a6474f10d24aa74d1b591ea
emir57 <emir.gurbuz06@hotmail.com>
2022-08-11 16:46:23 +0300

    Create BusinessRule

---
+++ Core/Utilities/Business/BusinessRule.cs
@@ -0,0 +1,15 @@
+﻿using Core.Utilities.Result;
+
+namespace Core.Utilities.Business
+{
+    public class BusinessRule
+    {
+        public static IResult Run(params IResult[] results)
+        {
+            foreach (IResult result in results)
+                if (result.Success == false)
+                    return result;
+            return null;
+        }
+    }
+}
