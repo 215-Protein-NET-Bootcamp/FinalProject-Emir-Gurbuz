e513efd4c6be702ff6f096d0bafd552ca92c3ccf
emir57 <emir.gurbuz06@hotmail.com>
2022-08-11 18:04:42 +0300

    Create LoginDto and RegisterDto

---
+++ Core/Dto/Concrete/LoginDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.Dto.Concrete
+{
+    public class LoginDto : IDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Core/Dto/Concrete/RegisterDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Core.Dto.Concrete
+{
+    public class RegisterDto : IDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
