f11e0dd2100c1ce471f9b541dc9df4f5c5e6a996
emir57 <emir.gurbuz06@hotmail.com>
2022-08-11 05:06:52 +0300

    CategoryReadDto and CategoryWriteDto

---
+++ NTech.Dto/Concrete/Category/CategoryReadDto.cs
@@ -0,0 +1,12 @@
+﻿using Core.Dto;
+
+namespace NTech.Dto.Concrete.Category
+{
+    public class CategoryReadDto : IReadDto
+    {
+        public string Name { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public DateTime? DeletedDate { get; set; }
+    }
+}
---
+++ NTech.Dto/Concrete/Category/CategoryWriteDto.cs
@@ -0,0 +1,9 @@
+﻿using Core.Dto;
+
+namespace NTech.Dto.Concrete.Category
+{
+    public class CategoryWriteDto : IWriteDto
+    {
+        public string Name { get; set; }
+    }
+}
