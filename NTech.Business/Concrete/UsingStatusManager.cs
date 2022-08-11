553263f13cac5d27bfd0bfe60c85bf1599729f1c
emir57 <emir.gurbuz06@hotmail.com>
2022-08-11 22:18:37 +0300

    UsingStatusWriteDtoValidator And Use

--- NTech.Business/Concrete/UsingStatusManager.cs
+++ NTech.Business/Concrete/UsingStatusManager.cs
@@ -1,6 +1,9 @@
 ﻿using AutoMapper;
+using Core.Aspect.Autofac.Validation;
+using Core.Utilities.Result;
 using Core.Utilities.ResultMessage;
 using NTech.Business.Abstract;
+using NTech.Business.Validators.FluentValidation;
 using NTech.DataAccess.Abstract;
 using NTech.DataAccess.UnitOfWork.Abstract;
 using NTech.Dto.Concrete;
@@ -12,6 +15,17 @@ namespace NTech.Business.Concrete
     {
         public UsingStatusManager(IUsingStatusDal repository, IMapper mapper, IUnitOfWork unitOfWork, ILanguageMessage languageMessage) : base(repository, mapper, unitOfWork, languageMessage)
         {
+        }
+
+        [ValidationAspect(typeof(UsingStatusWriteDtoValidator))]
+        public override Task<IResult> AddAsync(UsingStatusWriteDto dto)
+        {
+            return base.AddAsync(dto);
+        }
+        [ValidationAspect(typeof(UsingStatusWriteDtoValidator))]
+        public override Task<IResult> UpdateAsync(int id, UsingStatusWriteDto dto)
+        {
+            return base.UpdateAsync(id, dto);
         }
     }
 }
---
+++ NTech.Business/Validators/FluentValidation/UsingStatusWriteDtoValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using NTech.Dto.Concrete;
+
+namespace NTech.Business.Validators.FluentValidation
+{
+    public class UsingStatusWriteDtoValidator : AbstractValidator<UsingStatusWriteDto>
+    {
+        public UsingStatusWriteDtoValidator()
+        {
+            RuleFor(b => b.Status)
+                .NotNull()
+                .NotEmpty();
+            RuleFor(b => b.Status)
+                .MaximumLength(50);
+        }
+    }
+}
