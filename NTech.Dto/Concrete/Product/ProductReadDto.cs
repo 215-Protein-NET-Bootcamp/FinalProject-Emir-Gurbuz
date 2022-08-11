61afe39fd5aa5ebb0ff216c433255edc2420af14
emir57 <emir.gurbuz06@hotmail.com>
2022-08-11 05:14:10 +0300

    ProductReadDto Properties

--- NTech.Dto/Concrete/Product/ProductReadDto.cs
+++ NTech.Dto/Concrete/Product/ProductReadDto.cs
@@ -5,13 +5,13 @@ namespace NTech.Dto.Concrete
 {
     public class ProductReadDto : IReadDto
     {
-        public Category Category { get; set; }
+        public CategoryReadDto Category { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public Color Color { get; set; }
-        public Brand Brand { get; set; }
-        public UsingStatus UsingStatus { get; set; }
-        public Image Image { get; set; }
+        public ColorReadDto Color { get; set; }
+        public BrandReadDto Brand { get; set; }
+        public UsingStatusReadDto UsingStatus { get; set; }
+        public ImageReadDto Image { get; set; }
         public decimal Price { get; set; }
         public bool isOfferable { get; set; }
         public byte Rating { get; set; }
