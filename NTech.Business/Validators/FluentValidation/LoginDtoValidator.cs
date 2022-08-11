da736ce3eaa1ed6b791c8af5157f268802d6e0c9
emir57 <emir.gurbuz06@hotmail.com>
2022-08-11 20:53:33 +0300

    RegisterDtoValidator and LoginDtoValidator

---
+++ NTech.Business/Validators/FluentValidation/LoginDtoValidator.cs
@@ -0,0 +1,26 @@
+﻿using Core.Dto.Concrete;
+using FluentValidation;
+
+namespace NTech.Business.Validators.FluentValidation
+{
+    public class LoginDtoValidator : AbstractValidator<LoginDto>
+    {
+        public LoginDtoValidator()
+        {
+            RuleFor(l => l.Email)
+                .NotEmpty()
+                .NotNull();
+            RuleFor(l => l.Email)
+                .EmailAddress();
+
+            RuleFor(l => l.Password)
+                .NotEmpty()
+                .NotNull();
+            RuleFor(l => l.Password)
+                .MaximumLength(20);
+            RuleFor(l => l.Password)
+                .MinimumLength(8);
+
+        }
+    }
+}
---
+++ NTech.Business/Validators/FluentValidation/RegisterDtoValidator.cs
@@ -0,0 +1,36 @@
+﻿using Core.Dto.Concrete;
+using FluentValidation;
+
+namespace NTech.Business.Validators.FluentValidation
+{
+    public class RegisterDtoValidator : AbstractValidator<RegisterDto>
+    {
+        public RegisterDtoValidator()
+        {
+            RuleFor(r => r.FirstName)
+                .NotNull()
+                .NotEmpty();
+
+            RuleFor(r => r.FirstName)
+                .MaximumLength(30);
+
+            RuleFor(r => r.LastName)
+                .NotNull()
+                .NotEmpty();
+
+            RuleFor(r => r.LastName)
+                .MaximumLength(30);
+
+            RuleFor(r => r.Email)
+                .NotNull()
+                .NotEmpty();
+            RuleFor(r => r.Email)
+                .EmailAddress();
+
+            RuleFor(r => r.DateOfBirth)
+                .NotNull()
+                .NotEmpty();
+
+        }
+    }
+}
