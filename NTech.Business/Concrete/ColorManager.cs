63b987482ee0dcefceb843679e8510f97fd22e01
emir57 <emir.gurbuz06@hotmail.com>
2022-08-11 22:12:12 +0300

    ColorWriteDtoValidator And Use

--- NTech.Business/Concrete/ColorManager.cs
+++ NTech.Business/Concrete/ColorManager.cs
@@ -1,6 +1,9 @@
 ﻿using AutoMapper;
+using Core.Aspect.Autofac.Validation;
+using Core.Utilities.Result;
 using Core.Utilities.ResultMessage;
 using NTech.Business.Abstract;
+using NTech.Business.Validators.FluentValidation;
 using NTech.DataAccess.Abstract;
 using NTech.DataAccess.UnitOfWork.Abstract;
 using NTech.Dto.Concrete;
@@ -12,6 +15,18 @@ namespace NTech.Business.Concrete
     {
         public ColorManager(IColorDal repository, IMapper mapper, IUnitOfWork unitOfWork, ILanguageMessage languageMessage) : base(repository, mapper, unitOfWork, languageMessage)
         {
+        }
+
+        [ValidationAspect(typeof(ColorWriteDtoValidator))]
+        public override Task<IResult> AddAsync(ColorWriteDto dto)
+        {
+            return base.AddAsync(dto);
+        }
+
+        [ValidationAspect(typeof(ColorWriteDtoValidator))]
+        public override Task<IResult> UpdateAsync(int id, ColorWriteDto dto)
+        {
+            return base.UpdateAsync(id, dto);
         }
     }
 }
