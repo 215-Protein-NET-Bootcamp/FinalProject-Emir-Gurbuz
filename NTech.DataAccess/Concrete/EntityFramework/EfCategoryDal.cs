2d9d7793210233518f186b139111027fe78e5a6d
emir57 <emir.gurbuz06@hotmail.com>
2022-08-11 05:45:28 +0300

    EfCategoryDal -> Override GetAsync

--- NTech.DataAccess/Concrete/EntityFramework/EfCategoryDal.cs
+++ NTech.DataAccess/Concrete/EntityFramework/EfCategoryDal.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using NTech.DataAccess.Abstract;
 using NTech.Entity.Concrete;
+using System.Linq.Expressions;
 
 namespace NTech.DataAccess.Concrete.EntityFramework
 {
@@ -9,6 +10,14 @@ namespace NTech.DataAccess.Concrete.EntityFramework
     {
         public EfCategoryDal(DbContext dbContext) : base(dbContext)
         {
+        }
+        public override async Task<Category> GetAsync(Expression<Func<Category, bool>> predicate, bool tracking = true)
+        {
+            var table = _context.Set<Category>()
+                .Include(c => c.Products);
+            return tracking ?
+                await table.SingleOrDefaultAsync(predicate) :
+                await table.AsNoTracking().SingleOrDefaultAsync(predicate);
         }
     }
 }
