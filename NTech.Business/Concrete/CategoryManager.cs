33f62e705ab9e4d071c61ff5e95dd172338c061d
emir57 <emir.gurbuz06@hotmail.com>
2022-08-11 22:10:37 +0300

    CategoryWriteDtoValidator And Use

--- NTech.Business/Concrete/CategoryManager.cs
+++ NTech.Business/Concrete/CategoryManager.cs
@@ -1,6 +1,9 @@
 ﻿using AutoMapper;
+using Core.Aspect.Autofac.Validation;
+using Core.Utilities.Result;
 using Core.Utilities.ResultMessage;
 using NTech.Business.Abstract;
+using NTech.Business.Validators.FluentValidation;
 using NTech.DataAccess.Abstract;
 using NTech.DataAccess.UnitOfWork.Abstract;
 using NTech.Dto.Concrete;
@@ -12,6 +15,17 @@ namespace NTech.Business.Concrete
     {
         public CategoryManager(ICategoryDal repository, IMapper mapper, IUnitOfWork unitOfWork, ILanguageMessage languageMessage) : base(repository, mapper, unitOfWork, languageMessage)
         {
+        }
+
+        [ValidationAspect(typeof(CategoryWriteDtoValidator))]
+        public override Task<IResult> AddAsync(CategoryWriteDto dto)
+        {
+            return base.AddAsync(dto);
+        }
+        [ValidationAspect(typeof(CategoryWriteDtoValidator))]
+        public override Task<IResult> UpdateAsync(int id, CategoryWriteDto dto)
+        {
+            return base.UpdateAsync(id, dto);
         }
     }
 }
---
+++ NTech.Business/Validators/FluentValidation/CategoryWriteDtoValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using NTech.Dto.Concrete;
+
+namespace NTech.Business.Validators.FluentValidation
+{
+    public class CategoryWriteDtoValidator : AbstractValidator<CategoryWriteDto>
+    {
+        public CategoryWriteDtoValidator()
+        {
+            RuleFor(b => b.Name)
+                .NotNull()
+                .NotEmpty();
+            RuleFor(b => b.Name)
+                .MaximumLength(50);
+        }
+    }
+}
