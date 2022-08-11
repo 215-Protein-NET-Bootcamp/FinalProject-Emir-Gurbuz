da8994e8cd44bb9087f9029ea7bec0d2d1256cf7
emir57 <emir.gurbuz06@hotmail.com>
2022-08-11 14:51:21 +0300

    Complete Category

---
+++ NTech.Business/Concrete/CategoryManager.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using NTech.Business.Abstract;
+using NTech.DataAccess.Abstract;
+using NTech.DataAccess.UnitOfWork.Abstract;
+using NTech.Dto.Concrete;
+using NTech.Entity.Concrete;
+
+namespace NTech.Business.Concrete
+{
+    public class CategoryManager : AsyncBaseService<Category, CategoryWriteDto, CategoryReadDto>, ICategoryService
+    {
+        public CategoryManager(ICategoryDal repository, IMapper mapper, IUnitOfWork unitOfWork) : base(repository, mapper, unitOfWork)
+        {
+        }
+    }
+}
---
+++ NTech.WebAPI/Controllers/CategoriesController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NTech.Business.Abstract;
+using NTech.Dto.Concrete;
+using NTech.Entity.Concrete;
+
+namespace NTech.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class CategoriesController : BaseController<Category, CategoryWriteDto, CategoryReadDto>
+    {
+        public CategoriesController(ICategoryService baseService) : base(baseService)
+        {
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return await base.GetListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get([FromRoute] int id)
+        {
+            return await base.GetByIdAsync(id);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] CategoryWriteDto categoryWriteDto)
+        {
+            return await base.AddAsync(categoryWriteDto);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put([FromRoute] int id, [FromBody] CategoryWriteDto categoryWriteDto)
+        {
+            return await base.UpdateAsync(id, categoryWriteDto);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete([FromRoute] int id)
+        {
+            return await base.SoftDeleteAsync(id);
+        }
+    }
+}
