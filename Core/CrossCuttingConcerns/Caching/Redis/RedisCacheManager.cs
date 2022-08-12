3c6ce0cd788fceab98e7d8ce6d1673e7e3812a24
emir57 <emir.gurbuz06@hotmail.com>
2022-08-12 15:36:50 +0300

    Complete RedisCacheManager

---
+++ Core/CrossCuttingConcerns/Caching/Redis/RedisCacheManager.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Extensions.Configuration;
+using ServiceStack.Redis;
+
+namespace Core.CrossCuttingConcerns.Caching.Redis
+{
+    public class RedisCacheManager : ICacheManager
+    {
+        private readonly RedisConfiguration _redisConfiguration;
+        public RedisCacheManager(IConfiguration configuration)
+        {
+            _redisConfiguration = configuration.GetSection("RedisConfiguration").Get<RedisConfiguration>();
+        }
+
+        public void Add(string key, object value, int duration)
+        {
+            redisInvoker((x) => x.Add(key, value, TimeSpan.FromMinutes(duration)));
+        }
+
+        public object Get(string key)
+        {
+            object result = default;
+            redisInvoker((x) => result = x.Get(key));
+            return result;
+        }
+
+        public T Get<T>(string key)
+        {
+            T result = default;
+            redisInvoker((X) => result = X.Get<T>(key));
+            return result;
+        }
+
+        public bool IsAdd(string key)
+        {
+            bool isAdd = false;
+            redisInvoker((x) => isAdd = x.ContainsKey(key));
+            return isAdd;
+        }
+
+        public void Remove(string key)
+        {
+            redisInvoker((x) => x.Remove(key));
+        }
+
+        public void RemoveByPattern(string pattern)
+        {
+            redisInvoker((x) => x.RemoveByPattern(pattern));
+        }
+
+        private void redisInvoker(Action<RedisClient> redisAction)
+        {
+            using (var client = new RedisClient(_redisConfiguration.Host, _redisConfiguration.Port, _redisConfiguration.Password))
+            {
+                redisAction.Invoke(client);
+            }
+        }
+    }
+}
---
+++ Core/CrossCuttingConcerns/Caching/Redis/RedisConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.CrossCuttingConcerns.Caching.Redis
+{
+    public class RedisConfiguration
+    {
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public string Password { get; set; }
+    }
+}
