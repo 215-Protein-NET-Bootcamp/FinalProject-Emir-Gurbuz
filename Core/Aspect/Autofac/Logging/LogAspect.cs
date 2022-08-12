cc2724585c9cd4b54e0d1721e29962581099fd75
emir57 <emir.gurbuz06@hotmail.com>
2022-08-12 16:08:11 +0300

    Complete LogAspect

---
+++ Core/Aspect/Autofac/Logging/LogAspect.cs
@@ -0,0 +1,47 @@
+﻿using Castle.DynamicProxy;
+using Core.CrossCuttingConcerns.Logging;
+using Core.CrossCuttingConcerns.Logging.Serilog;
+using Core.Exceptions.Aspect;
+using Core.Utilities.Interceptor;
+using Core.Utilities.IoC;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Core.Aspect.Autofac.Logging
+{
+    public class LogAspect : MethodInterception
+    {
+        private readonly LoggerServiceBase _loggerServiceBase;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public LogAspect(Type loggingtype)
+        {
+            WrongLoggingTypeException.ThrowIfNotEqual(typeof(LoggerServiceBase), loggingtype);
+
+            _loggerServiceBase = (LoggerServiceBase)Activator.CreateInstance(loggingtype);
+            _httpContextAccessor = ServiceTool.ServiceProvider.GetService<IHttpContextAccessor>();
+        }
+
+        protected override void OnBefore(IInvocation invocation)
+        {
+            _loggerServiceBase.Info(getMethodDetail(invocation));
+        }
+
+        private string getMethodDetail(IInvocation invocation)
+        {
+            List<LogParameter> logParameters = invocation.Arguments.Select((a, i) => new LogParameter
+            {
+                Name = invocation.GetConcreteMethod().GetParameters()[i].Name,
+                Value = a,
+                Type = a.GetType().ToString()
+            }).ToList();
+            //TODO: user info
+            LogDetail logDetail = new()
+            {
+                MethodName = invocation.Method.Name,
+                Parameters = logParameters
+            };
+            return JsonConvert.SerializeObject(logDetail);
+        }
+    }
+}
