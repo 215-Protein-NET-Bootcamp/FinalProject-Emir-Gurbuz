23d1b0c650f705d42a3c5326a5b9b9451791c6c7
emir57 <emir.gurbuz06@hotmail.com>
2022-08-12 15:59:00 +0300

    Create WrongLoggingTypeException

---
+++ Core/Exceptions/Aspect/WrongLoggingTypeException.cs
@@ -0,0 +1,25 @@
+﻿using Core.Utilities.Messages;
+
+namespace Core.Exceptions.Aspect
+{
+    public class WrongLoggingTypeException : Exception
+    {
+        public WrongLoggingTypeException() : base(ExceptionMessages.WrongLoggingType)
+        {
+        }
+
+        public WrongLoggingTypeException(string? message) : base(message)
+        {
+        }
+
+        public WrongLoggingTypeException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        public static void ThrowIfNotEqual(Type argument1, Type argument2)
+        {
+            if (argument1.IsAssignableFrom(argument2) == false)
+                throw new WrongLoggingTypeException();
+        }
+    }
+}
