93d86128ae2b95afb8c7446649d998e26931a38e
emir57 <emir.gurbuz06@hotmail.com>
2022-08-12 15:38:20 +0300

    Cache Add Singleton in CoreModule

--- Core/DependencyResolvers/CoreModule.cs
+++ Core/DependencyResolvers/CoreModule.cs
@@ -1,4 +1,7 @@
-﻿using Core.Utilities.IoC;
+﻿using Core.CrossCuttingConcerns.Caching;
+using Core.CrossCuttingConcerns.Caching.Microsoft;
+using Core.CrossCuttingConcerns.Caching.Redis;
+using Core.Utilities.IoC;
 using Core.Utilities.MessageBrokers.RabbitMq;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
@@ -9,7 +12,14 @@ namespace Core.DependencyResolvers
     {
         public void Load(IServiceCollection services)
         {
+            #region HttpContextAccessor
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            #endregion
+
+            #region Cache
+            //services.AddMemoryCache();
+            services.AddSingleton<ICacheManager, RedisCacheManager>();
+            #endregion
         }
     }
 }
