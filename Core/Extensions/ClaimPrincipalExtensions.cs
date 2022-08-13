eebb9832636ddeb2cb0ea5d16c961a18ab7a4cb4
emir57 <emir.gurbuz06@hotmail.com>
2022-08-13 12:52:56 +0300

    ClaimPrincipalExtensions

---
+++ Core/Extensions/ClaimPrincipalExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Claims;
+
+namespace Core.Extensions
+{
+    public static class ClaimPrincipalExtensions
+    {
+        public static List<string> Claims(this ClaimsPrincipal claimsPrincipal, string type)
+        {
+            return claimsPrincipal?.FindAll(type)?.Select(x => x.Value).ToList();
+        }
+
+        public static List<string> ClaimRoles(this ClaimsPrincipal claimsPrincipal)
+        {
+            return claimsPrincipal?.Claims(ClaimTypes.Role).ToList();
+        }
+
+        public static string ClaimEmail(this ClaimsPrincipal claimsPrincipal)
+        {
+            return claimsPrincipal?.FindFirst(ClaimTypes.Email)?.Value ?? "<Anonymous>";
+        }
+    }
+}
