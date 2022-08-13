50da18e10ffa34f6e6b40dc921776b65debadb14
emir57 <emir.gurbuz06@hotmail.com>
2022-08-13 18:00:00 +0300

    Complete password hash tests

---
+++ NTech.Test/Helpers/HelpersTest.cs
@@ -0,0 +1,33 @@
+﻿using Core.Utilities.Security.Hashing;
+
+namespace NTech.Test.Helpers
+{
+    public class HelpersTest
+    {
+        public HelpersTest()
+        {
+
+        }
+
+        [Theory, InlineData("123456")]
+        public void Password_hash_test_true(string password)
+        {
+            byte[] passwordHash, passwordSalt;
+
+            HashingHelper.CreatePasswordHash(password, out passwordHash, out passwordSalt);
+            bool result = HashingHelper.VerifyPasswordHash(password, passwordHash, passwordSalt);
+
+            Assert.True(result);
+        }
+        [Theory, InlineData("123456", "654321")]
+        public void Password_hash_test_false(string password1, string password2)
+        {
+            byte[] passwordHash, passwordSalt;
+            HashingHelper.CreatePasswordHash(password2, out passwordHash, out passwordSalt);
+
+            bool result = HashingHelper.VerifyPasswordHash(password1, passwordHash, passwordSalt);
+
+            Assert.False(result);
+        }
+    }
+}
