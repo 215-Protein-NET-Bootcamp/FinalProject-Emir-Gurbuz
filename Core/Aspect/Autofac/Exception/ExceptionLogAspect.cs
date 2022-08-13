c08c6e15a4462f1547ab2feb88129f7300f7bce1
emir57 <emir.gurbuz06@hotmail.com>
2022-08-13 16:49:00 +0300

    ExceptionLogAspect

---
+++ Core/Aspect/Autofac/Exception/ExceptionLogAspect.cs
@@ -0,0 +1,44 @@
+﻿using Castle.DynamicProxy;
+using Core.CrossCuttingConcerns.Logging;
+using Core.CrossCuttingConcerns.Logging.Serilog;
+using Core.Exceptions.Aspect;
+using Core.Utilities.Interceptor;
+using Newtonsoft.Json;
+
+namespace Core.Aspect.Autofac.Exception
+{
+    public class ExceptionLogAspect : MethodInterception
+    {
+        private readonly LoggerServiceBase _loggerServiceBase;
+        public ExceptionLogAspect(Type loggingType)
+        {
+            WrongLoggingTypeException.ThrowIfNotEqual(typeof(LoggerServiceBase), loggingType);
+
+            _loggerServiceBase = (LoggerServiceBase)Activator.CreateInstance(loggingType);
+        }
+
+        protected override void OnException(IInvocation invocation, System.Exception e)
+        {
+            _loggerServiceBase.Error(getMethodDetail(invocation, e));
+        }
+
+        private string getMethodDetail(IInvocation invocation, System.Exception e)
+        {
+            List<LogParameter> parameters = invocation.Arguments.Select((a, i) => new LogParameter
+            {
+                Name = invocation.GetConcreteMethod().GetParameters()[i].Name,
+                Type = a.GetType().ToString(),
+                Value = a
+            }).ToList();
+
+            LogDetailWithException logDetailWithException = new LogDetailWithException
+            {
+                ExceptionMessage = e.Message,
+                MethodName = invocation.Method.Name,
+                Parameters = parameters
+            };
+
+            return JsonConvert.SerializeObject(logDetailWithException);
+        }
+    }
+}
