2b9d9acdfdcf0c2cdb9a29c86bd9a1cfe31f3bf1
emir57 <emir.gurbuz06@hotmail.com>
2022-08-13 12:47:00 +0300

    SerilogNotFoundFolderPathException

---
+++ Core/Exceptions/Logging/SerilogNotFoundFolderPathException.cs
@@ -0,0 +1,20 @@
+﻿using Core.Utilities.Messages;
+using System.Runtime.Serialization;
+
+namespace Core.Exceptions.Log
+{
+    public class SerilogNotFoundFolderPathException : Exception
+    {
+        public SerilogNotFoundFolderPathException() : base(ExceptionMessages.SerilogNotFoundFolderPath)
+        {
+        }
+
+        public SerilogNotFoundFolderPathException(string? message) : base(message)
+        {
+        }
+
+        protected SerilogNotFoundFolderPathException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
