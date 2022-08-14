e02f7161766aa083c8fd5887d0d46b515b056dfe
emir57 <emir.gurbuz06@hotmail.com>
2022-08-14 20:45:36 +0300

    EmailQueueManager constructor

--- NTech.Business/Concrete/EmailQueueManager.cs
+++ NTech.Business/Concrete/EmailQueueManager.cs
@@ -1,5 +1,4 @@
 ﻿using AutoMapper;
-using Core.DataAccess;
 using Core.Entity.Concrete;
 using Core.Utilities.ResultMessage;
 using NTech.Business.Abstract;
@@ -10,7 +9,7 @@ namespace NTech.Business.Concrete
 {
     public class EmailQueueManager : AsyncBaseService<EmailQueue, EmailQueueWriteDto, EmailQueueReadDto>, IEmailQueueService
     {
-        public EmailQueueManager(IAsyncRepository<EmailQueue> repository, IMapper mapper, IUnitOfWork unitOfWork, ILanguageMessage languageMessage) : base(repository, mapper, unitOfWork, languageMessage)
+        public EmailQueueManager(IEmailQueueService repository, IMapper mapper, IUnitOfWork unitOfWork, ILanguageMessage languageMessage) : base(repository, mapper, unitOfWork, languageMessage)
         {
         }
     }
