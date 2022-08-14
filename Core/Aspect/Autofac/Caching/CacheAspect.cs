9735e6e5f36df9e0e646da52cb5dfc4aa13a10b8
emir57 <emir.gurbuz06@hotmail.com>
2022-08-14 12:46:31 +0300

    Upgrade All projects

--- Core/Aspect/Autofac/Caching/CacheAspect.cs
+++ Core/Aspect/Autofac/Caching/CacheAspect.cs
@@ -11,7 +11,7 @@ namespace Core.Aspect.Autofac.Caching
     {
         private readonly ICacheManager _cacheManager;
         private readonly int _duration;
-        public CacheAspect(int duration)
+        public CacheAspect(int duration = 60)
         {
             _duration = duration;
             _cacheManager = ServiceTool.ServiceProvider.GetService<ICacheManager>();
@@ -24,6 +24,31 @@ namespace Core.Aspect.Autofac.Caching
             if (_cacheManager.IsAdd(key))
             {
                 invocation.ReturnValue = _cacheManager.Get(key);
+                return;
+            }
+            invocation.Proceed();
+            _cacheManager.Add(key, invocation.ReturnValue, _duration);
+
+        }
+    }
+    public class CacheAspect<T> : MethodInterception
+    {
+        private readonly ICacheManager _cacheManager;
+        private readonly int _duration;
+        public CacheAspect(int duration = 60)
+        {
+            _duration = duration;
+            _cacheManager = ServiceTool.ServiceProvider.GetService<ICacheManager>();
+        }
+
+        public override void Intercept(IInvocation invocation)
+        {
+            string key = invocation.GenerateMethodKey();
+
+            if (_cacheManager.IsAdd(key))
+            {
+                var value = _cacheManager.Get<T>(key);
+                invocation.ReturnValue = Task.Run(() => value);
                 return;
             }
             invocation.Proceed();
