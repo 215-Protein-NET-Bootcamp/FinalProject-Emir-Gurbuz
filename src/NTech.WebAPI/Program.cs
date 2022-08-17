1dfbac0dc49f87f3975595b3d2eee9dd733f4df3
emir57 <emir.gurbuz06@hotmail.com>
2022-08-17 16:10:01 +0300

    add screenshot

--- src/NTech.WebAPI/Program.cs
+++ src/NTech.WebAPI/Program.cs
@@ -32,8 +32,8 @@ builder.Services.AddSwaggerGen();
 #region SqlContext, PostgreContext
 //builder.Services.AddDbContext<NTechDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer")));
 builder.Services.AddDbContext<NTechDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSql")));
+
 builder.Services.AddScoped<DbContext, NTechDbContext>();
-//builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 #endregion
 
 #region Identity
