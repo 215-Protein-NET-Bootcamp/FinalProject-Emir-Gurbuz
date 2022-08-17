734fd1931bc9a9571f0a420259bb325186f5b528
emir57 <emir.gurbuz06@hotmail.com>
2022-08-17 15:41:11 +0300

    AuthManagerTest Complete

---
+++ NTech.Test/Business/AuthManagerTest.cs
@@ -0,0 +1,149 @@
+﻿using Core.Dto.Concrete;
+using Core.Entity.Concrete;
+using Core.Utilities.Result;
+using Core.Utilities.Security.JWT;
+using Moq;
+using NTech.Business.Abstract;
+
+namespace NTech.Test.Business
+{
+    public class AuthManagerTest
+    {
+        Mock<IAuthService> _authServiceMock;
+        public AuthManagerTest()
+        {
+            _authServiceMock = new Mock<IAuthService>();
+        }
+
+        [Fact]
+        public async Task Create_access_token_success()
+        {
+            _authServiceMock.Setup(x => x.CreateAccessToken(It.IsAny<User>())).ReturnsAsync(new SuccessDataResult<AccessToken>());
+
+            var result = await _authServiceMock.Object.CreateAccessToken(user);
+
+            Assert.True(result.Success);
+        }
+        [Fact]
+        public async Task Create_access_token_error()
+        {
+            _authServiceMock.Setup(x => x.CreateAccessToken(It.IsAny<User>())).ReturnsAsync(new ErrorDataResult<AccessToken>());
+
+            var result = await _authServiceMock.Object.CreateAccessToken(user);
+
+            Assert.False(result.Success);
+        }
+
+        [Fact]
+        public async Task Login_success()
+        {
+            _authServiceMock.Setup(x => x.LoginAsync(It.IsAny<LoginDto>())).ReturnsAsync(new SuccessDataResult<AccessToken>());
+
+            var result = await _authServiceMock.Object.LoginAsync(loginDto);
+
+            Assert.True(result.Success);
+        }
+        [Fact]
+        public async Task Login_error()
+        {
+            _authServiceMock.Setup(x => x.LoginAsync(It.IsAny<LoginDto>())).ReturnsAsync(new ErrorDataResult<AccessToken>());
+
+            var result = await _authServiceMock.Object.LoginAsync(loginDto);
+
+            Assert.False(result.Success);
+        }
+
+        [Fact]
+        public async Task Register_success()
+        {
+            _authServiceMock.Setup(x => x.RegisterAsync(It.IsAny<RegisterDto>())).ReturnsAsync(new SuccessDataResult<AccessToken>());
+
+            var result = await _authServiceMock.Object.RegisterAsync(registerDto);
+
+            Assert.True(result.Success);
+        }
+        [Fact]
+        public async Task Register_error()
+        {
+            _authServiceMock.Setup(x => x.RegisterAsync(It.IsAny<RegisterDto>())).ReturnsAsync(new ErrorDataResult<AccessToken>());
+
+            var result = await _authServiceMock.Object.RegisterAsync(registerDto);
+
+            Assert.False(result.Success);
+        }
+
+        [Fact]
+        public async Task Reset_password_success()
+        {
+            _authServiceMock.Setup(x => x.ResetPasswordAsync(It.IsAny<ResetPasswordDto>())).ReturnsAsync(new SuccessResult());
+
+            var result = await _authServiceMock.Object.ResetPasswordAsync(resetPasswordDto);
+
+            Assert.True(result.Success);
+        }
+        [Fact]
+        public async Task Reset_password_error()
+        {
+            _authServiceMock.Setup(x => x.ResetPasswordAsync(It.IsAny<ResetPasswordDto>())).ReturnsAsync(new ErrorResult());
+
+            var result = await _authServiceMock.Object.ResetPasswordAsync(resetPasswordDto);
+
+            Assert.False(result.Success);
+        }
+
+        private LoginDto loginDto
+        {
+            get
+            {
+                return new()
+                {
+                    Email = "username@hotmail.com",
+                    Password = "123456Aa"
+                };
+            }
+        }
+        private RegisterDto registerDto
+        {
+            get
+            {
+                return new()
+                {
+                    FirstName = "Emir",
+                    LastName = "Gürbüz",
+                    Email = "username@hotmail.com",
+                    Password = "123456Aa",
+                    DateOfBirth = new DateTime(2002, 9, 8)
+                };
+            }
+        }
+        private User user
+        {
+            get
+            {
+                return new()
+                {
+                    Id = 1,
+                    FirstName = "Emir",
+                    LastName = "Gürbüz",
+                    Email = "username@hotmail.com",
+                    PasswordHash = new byte[1],
+                    PasswordSalt = new byte[1],
+                    Status = true,
+                    DateOfBirth = new DateTime(2002, 9, 8)
+                };
+            }
+        }
+        private ResetPasswordDto resetPasswordDto
+        {
+            get
+            {
+                return new()
+                {
+                    NewPassword = "1234567Aa",
+                    OldPassword = "123456Aa",
+                    UserId = 1
+                };
+            }
+        }
+    }
+}
--- NTech.Test/Business/ProductManagerTest.cs
+++ NTech.Test/Business/ProductManagerTest.cs
@@ -1,21 +1,93 @@
-﻿using Moq;
+﻿using Core.Utilities.Result;
+using Moq;
 using NTech.Business.Abstract;
-using NTech.Business.Concrete;
+using NTech.Dto.Concrete;
 
 namespace NTech.Test.Business
 {
     public class ProductManagerTest
     {
-        Mock<IProductService> _productService;
+        Mock<IProductService> _productServiceMock;
         public ProductManagerTest()
         {
-
+            _productServiceMock = new Mock<IProductService>();
         }
 
         [Fact]
-        public void Test()
+        public async Task Product_add_success()
         {
-            _productService.Object.BuyAsync(1);
+            _productServiceMock.Setup(x => x.AddAsync(It.IsAny<ProductWriteDto>())).ReturnsAsync(new SuccessResult());
+
+            var result = await _productServiceMock.Object.AddAsync(productWriteDto);
+
+            Assert.True(result.Success);
+        }
+        [Fact]
+        public async Task Product_add_error()
+        {
+            _productServiceMock.Setup(x => x.AddAsync(It.IsAny<ProductWriteDto>())).ReturnsAsync(new ErrorResult());
+
+            var result = await _productServiceMock.Object.AddAsync(productWriteDto);
+
+            Assert.False(result.Success);
+        }
+
+        [Theory, InlineData(1)]
+        public async Task Product_update_success(int id)
+        {
+            _productServiceMock.Setup(x => x.UpdateAsync(It.IsAny<int>(), It.IsAny<ProductWriteDto>())).ReturnsAsync(new SuccessResult());
+
+            var result = await _productServiceMock.Object.UpdateAsync(id, productWriteDto);
+
+            Assert.True(result.Success);
+        }
+        [Theory, InlineData(1)]
+        public async Task Product_update_error(int id)
+        {
+            _productServiceMock.Setup(x => x.UpdateAsync(It.IsAny<int>(), It.IsAny<ProductWriteDto>())).ReturnsAsync(new ErrorResult());
+
+            var result = await _productServiceMock.Object.UpdateAsync(id, productWriteDto);
+
+            Assert.False(result.Success);
+        }
+
+        [Theory, InlineData(1)]
+        public async Task Product_Buy_success(int id)
+        {
+            _productServiceMock.Setup(x => x.BuyAsync(It.IsAny<int>())).ReturnsAsync(new SuccessResult());
+
+            var result = await _productServiceMock.Object.BuyAsync(id);
+
+            Assert.True(result.Success);
+        }
+        [Theory, InlineData(1)]
+        public async Task Product_Buy_error(int id)
+        {
+            _productServiceMock.Setup(x => x.BuyAsync(It.IsAny<int>())).ReturnsAsync(new ErrorResult());
+
+            var result = await _productServiceMock.Object.BuyAsync(id);
+
+            Assert.False(result.Success);
+        }
+
+        private static ProductWriteDto productWriteDto
+        {
+            get
+            {
+                return new()
+                {
+                    Name = "name",
+                    Description = "description",
+                    Price = 100,
+                    isOfferable = false,
+                    Rating = 4,
+                    BrandId = 1,
+                    CategoryId = 1,
+                    ColorId = 1,
+                    ImageId = 1,
+                    UsingStatusId = 1
+                };
+            }
         }
     }
 }
