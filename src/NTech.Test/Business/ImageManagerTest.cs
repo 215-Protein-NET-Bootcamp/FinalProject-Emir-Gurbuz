afb2098f2483fa10f9ed431065a4e9d062e5ebea
emir57 <emir.gurbuz06@hotmail.com>
2022-08-18 14:53:21 +0300

    ImageManagerTest

---
+++ src/NTech.Test/Business/ImageManagerTest.cs
@@ -0,0 +1,38 @@
+﻿using Core.Utilities.Result;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using NTech.Business.Abstract;
+using NTech.Entity.Concrete;
+
+namespace NTech.Test.Business
+{
+    public class ImageManagerTest
+    {
+        Mock<IImageService> _imageServiceMock;
+        Mock<IFormFile> _formFileMock;
+        public ImageManagerTest()
+        {
+            _imageServiceMock = new Mock<IImageService>();
+            _formFileMock = new Mock<IFormFile>();
+        }
+
+        [Fact]
+        public async Task Upload_image_success()
+        {
+            _imageServiceMock.Setup(x => x.UploadAsync(It.IsAny<IFormFile>())).ReturnsAsync(new SuccessDataResult<Image>());
+
+            var result = await _imageServiceMock.Object.UploadAsync(_formFileMock.Object);
+
+            Assert.True(result.Success);
+        }
+        [Fact]
+        public async Task Upload_image_error()
+        {
+            _imageServiceMock.Setup(x => x.UploadAsync(It.IsAny<IFormFile>())).ReturnsAsync(new ErrorDataResult<Image>());
+
+            var result = await _imageServiceMock.Object.UploadAsync(_formFileMock.Object);
+
+            Assert.False(result.Success);
+        }
+    }
+}
