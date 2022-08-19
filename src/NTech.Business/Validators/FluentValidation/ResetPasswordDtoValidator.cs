17f3a429eb29f27d27c88596d78876c5896aaaa4
emir57 <emir.gurbuz06@hotmail.com>
2022-08-19 13:37:26 +0300

    ResetPasswordDtoValidator

---
+++ src/NTech.Business/Validators/FluentValidation/ResetPasswordDtoValidator.cs
@@ -0,0 +1,31 @@
+﻿using Core.Dto.Concrete;
+using FluentValidation;
+
+namespace NTech.Business.Validators.FluentValidation
+{
+    public class ResetPasswordDtoValidator : AbstractValidator<ResetPasswordDto>
+    {
+        public ResetPasswordDtoValidator()
+        {
+            RuleFor(r => r.UserId)
+                .NotEmpty()
+                .NotNull();
+
+            RuleFor(r => r.OldPassword)
+                .NotEmpty()
+                .NotNull();
+            RuleFor(l => l.OldPassword)
+                .MaximumLength(20);
+            RuleFor(l => l.OldPassword)
+                .MinimumLength(8);
+
+            RuleFor(r => r.NewPassword)
+                .NotEmpty()
+                .NotNull();
+            RuleFor(l => l.NewPassword)
+                .MaximumLength(20);
+            RuleFor(l => l.NewPassword)
+                .MinimumLength(8);
+        }
+    }
+}
